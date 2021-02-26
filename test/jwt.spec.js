1d4c226f6fa510b091f9b271d0dd6ea959c3361d
Tor Hagemann <tor.hagemann@socotra.com>
2021-02-25 17:06:56 -0800

    test: add basic spec for JWT module

---
+++ test/jwt.spec.js
@@ -0,0 +1,49 @@
+/* globals describe, it */
+const assert = require('assert')
+const crypto = require('crypto')
+
+const JWT = require('../lib/jwt.js')
+
+const rsaPublic = (key, type = 'pkcs1') => {
+  const pub = crypto.createPublicKey({
+    format: 'pem',
+    key,
+  })
+  return pub.export({
+    format: 'pem',
+    type,
+  })
+}
+
+describe('lib', function () {
+  it('supports the HS256 algorithm (for JWTs)', function () {
+    const secret = process.env.HS256_SECRET || JWT._genKey('HS256')
+    const who = process.env.USER || 'nobody'
+    const now = Math.round(Date.now() / 1000)
+    const expires = 3600
+    const expected = {
+      aud: who,
+      exp: expires,
+      iat: now,
+      iss: who,
+      jti: who,
+      //nbf: now + 60, // nbf = not before (future)
+      sub: who,
+    }
+    const output = JWT._create('HS256', secret, expected)
+    const [data] = JWT._verify(output, secret, 'HS256')
+    const actual = Object.assign(data, { exp: expires })
+    assert.deepStrictEqual(actual, expected)
+  })
+
+  it('supports RSA key pairs, too', function () {
+    const secret = JWT._genKey('RS256')
+    const shared = rsaPublic(secret)
+    const input = {
+      socotra: 'account',
+    }
+    const output = JWT._create('RS256', secret, input)
+    const [data] = JWT._verify(output, shared, 'RS256')
+    assert.equal(data.socotra, input.socotra)
+  })
+})
